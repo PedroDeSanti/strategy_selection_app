0b322eb7b35a8e0b3a6412c38fdaf02ee9135f7a
PedroDeSanti <phmartinsanti@gmail.com>
2023-02-28 17:31:09 -0300

    ✨ add strategy related classes

---
+++ lib/models/startegy.dart
@@ -0,0 +1,13 @@
+class Strategy {
+  String name;
+  String description;
+  int id;
+
+  Strategy({required this.name, required this.description, required this.id});
+
+  // Strategy.fromJson(Map<String, dynamic> json) {
+  //   name = json['name'];
+  //   description = json['description'];
+  //   id = json['id'];
+  // }
+}
---
+++ lib/repositories/strategy_repository.dart
@@ -0,0 +1,81 @@
+import 'package:strategy_selection_app/models/startegy.dart';
+
+class StrategyRepository {
+  static List<Strategy> strategies = [
+    Strategy(
+      name: 'Estrela',
+      description: 'Description 1',
+      id: 1,
+    ),
+    Strategy(
+      name: 'Dibre Left',
+      description: 'Description 2',
+      id: 2,
+    ),
+    Strategy(
+      name: 'Dibre Right',
+      description: 'Description 3',
+      id: 3,
+    ),
+    Strategy(
+      name: 'Arco Esquerda',
+      description: 'Description 4',
+      id: 4,
+    ),
+    Strategy(
+      name: 'Arco Direita',
+      description: 'Description 5',
+      id: 5,
+    ),
+    Strategy(
+      name: 'Arco Esquerda Menor',
+      description: 'Description 6',
+      id: 6,
+    ),
+    Strategy(
+      name: 'Arco Direita Menor',
+      description: 'Description 7',
+      id: 7,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 8',
+      id: 8,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 9',
+      id: 9,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 10',
+      id: 10,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 11',
+      id: 11,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 12',
+      id: 12,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 13',
+      id: 13,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 14',
+      id: 14,
+    ),
+    Strategy(
+      name: 'Desempate',
+      description: 'Description 15',
+      id: 15,
+    ),
+  ];
+}
